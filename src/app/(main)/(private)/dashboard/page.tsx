6eb78ed683a39ed137d4b51a9161fc1523fe9ed1
JoanZapata05 <zapatajoan05@gmail.com>
2024-11-19 09:34:12 -0500

    feat:

---
+++ src/app/(main)/(private)/dashboard/page.tsx
@@ -0,0 +1,11 @@
+import DashboardPage from '@/ui/pages/DashboardPage'
+import React from 'react'
+
+export default function Dashboard() {
+  return (
+    <>
+    <DashboardPage/>
+    </>
+
+  )
+}
